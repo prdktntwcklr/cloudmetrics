b68a55135483b5978cdbfbae42d7d346504fc6c3
prdktntwcklr <61001903+prdktntwcklr@users.noreply.github.com>
2026-06-23 11:25:39 +0800

    fix: fix smoke test image name

--- app/api/smoke_test.go
+++ app/api/smoke_test.go
@@ -18,7 +18,7 @@ func TestHealthzSmoke(t *testing.T) {
 	ctx := context.Background()
 
 	req := testcontainers.ContainerRequest{
-		Image:        "cloudmetrics:local",
+		Image:        "cloudmetrics-app:local",
 		ExposedPorts: []string{"8080/tcp"},
 		WaitingFor: wait.ForHTTP("/healthz").
 			WithPort("8080/tcp").
@@ -49,7 +49,7 @@ func TestIngestReadingSmoke(t *testing.T) {
 	ctx := context.Background()
 
 	req := testcontainers.ContainerRequest{
-		Image:        "cloudmetrics:local",
+		Image:        "cloudmetrics-app:local",
 		ExposedPorts: []string{"8080/tcp"},
 		WaitingFor: wait.ForHTTP("/healthz").
 			WithPort("8080/tcp").
